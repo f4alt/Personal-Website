c528ea11b34202519a5bc3d8bece1d9cc05b05a5
Christopher <christopherjmacgregor@gmail.com>
2020-06-25 22:21:41 -0500

    app.listen cleanup

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get('*', function(req, res) {
 });
 
 port = process.env.PORT || 8080;
-ip = process.env.IP || '127.0.0.1';
 app.listen(port, function() {
   console.log('server started on ' + port);
   });
